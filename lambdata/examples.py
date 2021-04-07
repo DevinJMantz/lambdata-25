c6b3e031515bc45fe790772e75923925f3cea6a4
DevinJMantz <devinjmantz@gmail.com>
2021-04-07 15:32:29 -0400

    Modual 2 assignment

---
+++ lambdata/examples.py
@@ -0,0 +1,46 @@
+"""Module 2 OOPExample Module"""
+
+class BareMin:
+    pass
+
+class Complex:
+    """
+    This is a class with a real and imaginary attribute
+    """
+    def __init__(self, real, imag):
+        """
+        Constructor for complex numbers
+        """
+        self.r = real
+        self.i = imag
+
+    def add(self, complex_other):
+        """
+        Adds two ccomplex objects together
+        """
+        self.r += complex_other.r
+        self.i += complex_other.i
+    
+    def __repr__(self):
+        return '({},{})'.format(self.r, slef.i)
+
+    
+
+
+# complex_num_1 = Complex(3, -5)
+
+# complex_num_2 = Complex(2, 6)
+
+
+class Social:
+    def __init__(self, name, location, upvotes=0):
+        self.name = name
+        self.location = location
+        self.upvotes = upvotes
+
+    def recieve_upvotes(self, nun_upvotes):
+        self.upvotes += nun_upvotes
+
+    def is_popular(self):
+        return self.upvotes > 100
+    
--- lambdata/helper_functions.py
+++ lambdata/helper_functions.py
@@ -1,7 +1,10 @@
-def null_counts(df):
-    return df.isnull().sum().sum()
+import pandas as pd
 
-def train_test_split(df, frac):
-    cutoff = round(len(df) * frac)
-    return df[:cutoff], df[cutoff:]
+class MyDF(pd.DataFrame): 
+    def null_counts(df):
+        return df.isnull().sum().sum()
+
+    def train_test_split(df, frac):
+        cutoff = round(len(df) * frac)
+        return df[:cutoff], df[cutoff:]
     
